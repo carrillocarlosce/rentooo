463356caf74a93af0b86d29b4f00326f984e9d6c
carrillocarlosce <bestguru.peru@gmail.com>
2019-05-24 15:30:04 +0200

    login done

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,14 +1,16 @@
 /**
  * Login Screen
- * 
+ *
  * @format
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Text, View, TextInput, TouchableOpacity, Image} from 'react-native';
-import { Actions } from 'react-native-router-flux';
-import styles from '../style/loginStyle'
+import React, { Component } from "react";
+import { Text, View, TextInput, TouchableOpacity, Image } from "react-native";
+import { Actions } from "react-native-router-flux";
+import firebase from "react-native-firebase";
+
+import styles from "../style/loginStyle";
 
 export default class Login extends Component {
   constructor(props) {
@@ -16,37 +18,84 @@ export default class Login extends Component {
     this.state = {
       email: "",
       password: ""
-    }
+    };
   }
+
+  onLogin() {
+    const { email, password } = this.state;
+
+    console.log("salut");
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(function() {
+        let databaseRef = firebase.database().ref("users/");
+        databaseRef
+          .orderByChild("email")
+          .equalTo(email)
+          .on("child_added", function(userData) {
+            if (userData) {
+              let currentUser = userData.val();
+              currentUser["userID"] = userData.key;
+              window.currentUser = currentUser;
+
+              Actions.reset("dashboardContainerScreen");
+            }
+          });
+      })
+      .catch(error => {
+        console.log("login_error", error);
+      });
+  }
+
   render() {
     return (
-      <View style={{flex: 1, backgroundColor: "#ffffff"}}>
+      <View style={{ flex: 1, backgroundColor: "#ffffff" }}>
         <View style={styles.container}>
-
           <Text style={styles.welcomeText}>Welcome Back</Text>
 
           <View style={styles.formLayout}>
-            <TextInput style={styles.formItem} placeholder="Email Address" placeholderTextColor="#979797" value={this.state.email} onChangeText={(email) => this.setState({email})} />
-            <TextInput style={styles.formItem} placeholder="Password" secureTextEntry value={this.state.password} onChangeText={(password) => this.setState({password})}/>
-            <TouchableOpacity style={styles.forgotTextBtn} onPress={()=>Actions.Forgot()}>
+            <TextInput
+              autoCapitalize="none"
+              style={styles.formItem}
+              placeholder="Email Address"
+              placeholderTextColor="#979797"
+              value={this.state.email}
+              onChangeText={email => this.setState({ email })}
+            />
+            <TextInput
+              style={styles.formItem}
+              placeholder="Password"
+              secureTextEntry
+              value={this.state.password}
+              onChangeText={password => this.setState({ password })}
+            />
+            <TouchableOpacity
+              style={styles.forgotTextBtn}
+              onPress={() => Actions.Forgot()}
+            >
               <Text style={styles.forgotText}>Forgot your password?</Text>
             </TouchableOpacity>
           </View>
           <View>
-            <TouchableOpacity style={styles.btnLoginLayout} onPress={()=>Actions.dashboardContainerScreen()}>
-                <Text style={styles.btnLoginText}>Log in</Text>
+            <TouchableOpacity
+              style={styles.btnLoginLayout}
+              onPress={() => this.onLogin()}
+            >
+              <Text style={styles.btnLoginText}>Log in</Text>
             </TouchableOpacity>
             <Text style={styles.textLoginWithLayout}>or log in with</Text>
             <TouchableOpacity style={styles.btnFBLayout}>
-              <Image source={require('../../assets/images/facebook.png')}/>
+              <Image source={require("../../assets/images/facebook.png")} />
               <Text style={styles.btnText}>Facebook</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.btnTwitterLayout}>
-              <Image source={require('../../assets/images/twitter.png')}/>
+              <Image source={require("../../assets/images/twitter.png")} />
               <Text style={styles.btnText}>Twitter</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.btnGoogleLayout}>
-              <Image source={require('../../assets/images/google.png')}/>
+              <Image source={require("../../assets/images/google.png")} />
               <Text style={styles.btnText}>Google</Text>
             </TouchableOpacity>
           </View>
@@ -55,5 +104,3 @@ export default class Login extends Component {
     );
   }
 }
-
-
